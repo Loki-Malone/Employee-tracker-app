e1d6c10d96134d4906c77b2da22d7fa6d3093534
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-04-30 22:29:10 -0500

    created department,role,and employee tables in schema.sql

---
+++ DepartmentRoleAndEmployee-Schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+CREATE TABLE departments (
+     PRIMARY KEY(id)
+    name  VARCHAR(30) NULL,
+    id INT PRIMARY KEY,
+);
+
+CREATE TABLE role (
+     PRIMARY KEY(id)
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department id INT NOT NULL,
+    id PRIMARY KEY
+)
+
+    
+CREATE TABLE employee (
+    PRIMARY KEY(id)
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    id INT PRIMARY KEY
+    );
+
+    SELECT * FROM departments;
+    SELECT * from role;
+    SELECT * FROM employee;
+
