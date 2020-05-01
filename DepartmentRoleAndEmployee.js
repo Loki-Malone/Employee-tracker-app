bebd600c6e7eb59460ab07e58c37083bdcfbf27e
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-04-30 22:43:39 -0500

    made connection for function templates

--- DepartmentRoleAndEmployee.js
+++ DepartmentRoleAndEmployee.js
@@ -4,13 +4,15 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  
   port: 3306,
 
-  
   user: "root",
-
-  
+ 
   password: "",
   database: "eployee_trackerDB"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    makeTemplate();
+  });
